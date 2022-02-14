d91b82fba24e6ebfc0ae2389065dc30cce8629f8
vinayak ushakola <vinayak.ushakola@gmail.com>
2022-02-15 01:02:04 +0530

    Added recursive solution for palindrome problem

---
+++ Utility/Recursion/clsRecursion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Recursion
+{
+    public class clsRecursion
+    {
+        public static int IsPalindrome(string A, int start, int end)
+        {
+            if (start >= end)
+                return 1;
+            if (A[start] != A[end])
+                return 0;
+            return IsPalindrome(A, start + 1, end - 1);
+        }
+    }
+}
